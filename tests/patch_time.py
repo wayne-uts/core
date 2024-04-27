6ef5888323b7c68b430258f4ad686d40d62a6b94
Erik <erik@montnemery.com>
2024-04-27 10:27:20 +0200

    Don't patch homeassistant.runner.monotonic in tests

--- tests/patch_time.py
+++ tests/patch_time.py
@@ -3,9 +3,8 @@
 from __future__ import annotations
 
 import datetime
-import time
 
-from homeassistant import runner, util
+from homeassistant import util
 from homeassistant.util import dt as dt_util
 
 
@@ -14,11 +13,5 @@ def _utcnow() -> datetime.datetime:
     return datetime.datetime.now(datetime.UTC)
 
 
-def _monotonic() -> float:
-    """Make monotonic patchable by freezegun."""
-    return time.monotonic()
-
-
 dt_util.utcnow = _utcnow  # type: ignore[assignment]
 util.utcnow = _utcnow  # type: ignore[assignment]
-runner.monotonic = _monotonic  # type: ignore[assignment]
